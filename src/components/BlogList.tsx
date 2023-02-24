b523896bf01123f30ca424cfbf13827b44645487
Lucas Jansen <40787982+wlaj@users.noreply.github.com>
2023-02-24 10:16:56 +0100

    Create mapping of blogs

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   posts: any;
@@ -6,13 +7,15 @@ type Props = {
 
 export default function BlogList({ posts }: Props) {
   return (
-    <div>
+    <div className="grid mx-3 my-48 grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 gap-8">
       {posts.map((post: any) => (
         <div key={post.id}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img className="rounded-lg w-auto" src={post.feature_image} alt={post.feature_image_alt} />
           <Link href={`posts/${post.slug}`}>
-            <p>{post.title}</p>
+            <h4 className="font-semibold text-xl py-2">{post.title}</h4>
           </Link>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <p>{post.excerpt}</p>
         </div>
       ))}
     </div>
