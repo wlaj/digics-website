c6cf77989004bcad91d988ae6bb4ee37cbb345a0
Lucas Jansen <40787982+wlaj@users.noreply.github.com>
2023-03-03 10:12:42 +0100

    Update types and make small changes to styling

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -1,4 +1,37 @@
-export type Post = {
-    title: string
-    id: string
-}
+export type PostProps = {
+  post: {
+    title: string;
+    html: string;
+    published_at: string;
+    feature_image: string;
+    feature_image_alt: string;
+    excerpt: string;
+    og_image: string;
+    og_title: string;
+    og_description: string;
+    twitter_image: string;
+    twitter_title: string;
+    twitter_description: string;
+    meta_title: string;
+    meta_description: string;
+    url: string;
+  };
+};
+
+export type MetaTagsProps = {
+  meta_title: string;
+  meta_description: string;
+  og_title: string;
+  og_image: string;
+  og_description: string;
+  url: string;
+  twitter_title: string;
+  twitter_description: string;
+  twitter_image: string;
+};
+
+export type SettingsProps = {
+  settings: {
+    logo: string;
+  };
+};
