5c001cb59d6d632c09cabe1d74ea85552260e39b
Lucas Jansen <wlajansen16@gmail.com>
2023-03-03 14:14:55 +0000

    Add data to BlogList

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -19,7 +19,7 @@ const posts = [
   // More posts...
 ]
 
-export default function Example() {
+export default function BlogList({ data }) {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -30,40 +30,44 @@ export default function Example() {
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+          {data.map((item) => (
+            <article key={item.id} className="flex max-w-xl flex-col items-start justify-between">
+              <img src={item.feature_image} alt="" className="rounded-lg h-56 bg-gray-50" />
+
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-500">
-                  {post.date}
+                <time dateTime={item.datetime} className="text-gray-500">
+                  {item.updated_at}
                 </time>
+                {/* {data.tags.map((item) => (
                 <a
-                  href={post.category.href}
+                  href={item.slug}
                   className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
                 >
-                  {post.category.title}
+                  {item.name}
                 </a>
+                ))} */}
               </div>
               <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={post.href}>
+                <h3 className="mt-3 h-10 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={item.slug}>
                     <span className="absolute inset-0" />
-                    {post.title}
+                    {item.title}
                   </a>
                 </h3>
-                <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">{post.description}</p>
+                <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">{item.excerpt}</p>
               </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
-                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+              {/* <div className="relative mt-8 flex items-center gap-x-4">
+                {item.primary_author.profile_image && <img src={item.primary_author.profile_image} alt="" className="h-10 w-10 rounded-full bg-gray-50" />}
                 <div className="text-sm leading-6">
                   <p className="font-semibold text-gray-900">
-                    <a href={post.author.href}>
+                    <a href={item.primary_author.url}>
                       <span className="absolute inset-0" />
-                      {post.author.name}
+                      {item.primary_author.name}
                     </a>
                   </p>
-                  <p className="text-gray-600">{post.author.role}</p>
+                  <p className="text-gray-600">{item.primary_author.bio}</p>
                 </div>
-              </div>
+              </div> */}
             </article>
           ))}
         </div>
